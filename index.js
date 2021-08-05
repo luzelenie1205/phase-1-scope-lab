159881291232721a23fd3c8392ead6a137e7fd89
luzelenie <luzelenie1205@gmail.com>
2021-08-05 12:27:13 -0400

    commit tree 34bf1f55aacb67b4eadee08cd5fc8b1f146a936c

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName ='bob';
+const  leastFavoriteCustomer= "dan"
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+
+}
+ function setBestCustomer(){
+    bestCustomer="not bob"
+     return bestCustomer
+ }
+ function overwriteBestCustomer(){
+     bestCustomer="maybe bob"
+ }
+  
+ function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "daniel";
+    return leastFavoriteCustomer
+ }
